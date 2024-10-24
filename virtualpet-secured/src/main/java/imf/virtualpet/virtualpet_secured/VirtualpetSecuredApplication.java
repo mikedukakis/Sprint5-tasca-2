9aad2791b5cdc5615d9db0306d5ba8de52546b96
imf <imoyaf@gmail.com>
2024-10-24 11:25:20 +0200

    Initial commit

---
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/VirtualpetSecuredApplication.java
@@ -0,0 +1,13 @@
+package imf.virtualpet.virtualpet_secured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualpetSecuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualpetSecuredApplication.class, args);
+	}
+
+}
---
+++ virtualpet-secured/src/test/java/imf/virtualpet/virtualpet_secured/VirtualpetSecuredApplicationTests.java
@@ -0,0 +1,13 @@
+package imf.virtualpet.virtualpet_secured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualpetSecuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
