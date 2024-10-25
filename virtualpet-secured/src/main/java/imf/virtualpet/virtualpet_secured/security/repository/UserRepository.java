957e03277d355ab45c54c25fb60b83e9b0f2968f
imf <imoyaf@gmail.com>
2024-10-25 07:22:54 +0200

    Create User repository, add findByUserName custom method

---
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package imf.virtualpet.virtualpet_secured.security.repository;
+
+import imf.virtualpet.virtualpet_secured.security.entity.User;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+    Mono<User> findByUsername(String username);
+}
