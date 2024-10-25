731cd8db09d98318f38b34266af10d3a14cd6aca
imf <imoyaf@gmail.com>
2024-10-25 07:30:20 +0200

    Add secret key for token decoding

---
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/security/config/SecretKey.java
@@ -0,0 +1,12 @@
+package imf.virtualpet.virtualpet_secured.security.config;
+
+public class SecretKey {
+    private static final String SECRET_KEY = "Tl+wC+LcB+FwYMrjVAuXeuBmuecnxX8p3nPZU53BWsY=";
+
+    public SecretKey() {
+    }
+
+    public static String getSecretKey() {
+        return SECRET_KEY;
+    }
+}
