b0cb97749a18ea232a9bdae678d8be427ffd0436
imf <imoyaf@gmail.com>
2024-10-25 07:28:58 +0200

    Create InitialSetup class, with database and admin user initialisation

---
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/security/initialisation/InitialSetup.java
@@ -0,0 +1,36 @@
+package imf.virtualpet.virtualpet_secured.security.initialisation;
+
+import imf.virtualpet.virtualpet_secured.security.entity.Role;
+import imf.virtualpet.virtualpet_secured.security.entity.User;
+import imf.virtualpet.virtualpet_secured.security.service.UserService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class InitialSetup {
+    private static final String USER_COLLECTION = "user";
+    private static final String VIRTUALPET_COLLECTION = "virtualpet";
+
+    @Bean
+    public ApplicationRunner setupAdminUser(UserService userService, PasswordEncoder passwordEncoder, ReactiveMongoTemplate reactiveMongoTemplate) {
+        return args -> createDatabaseAndCollections(reactiveMongoTemplate)
+                .then(
+                        userService.findByUsername("admin")
+                                .switchIfEmpty(
+                                        userService.registerUser(new User("admin", passwordEncoder.encode("123456789"), Role.ADMIN))
+                                )
+                ).subscribe();
+    }
+
+    private Mono<Void> createDatabaseAndCollections(ReactiveMongoTemplate reactiveMongoTemplate) {
+        return reactiveMongoTemplate.createCollection(USER_COLLECTION)
+                .onErrorResume(e -> Mono.empty())
+                .then(reactiveMongoTemplate.createCollection(VIRTUALPET_COLLECTION)
+                        .onErrorResume(e -> Mono.empty()))
+                .then();
+    }
+}
