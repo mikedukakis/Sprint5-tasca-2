29a014555e0fdbccfd6ed6a1173b4e1f5c63747d
imf <imoyaf@gmail.com>
2024-10-27 22:54:34 +0100

    Add method to find by pet name

--- virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/repository/VirtualPetRepository.java
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/repository/VirtualPetRepository.java
@@ -3,6 +3,9 @@ package imf.virtualpet.virtualpet_secured.domain.repository;
 import imf.virtualpet.virtualpet_secured.domain.entity.VirtualPet;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface VirtualPetRepository extends ReactiveMongoRepository<VirtualPet, String> {}
+public interface VirtualPetRepository extends ReactiveMongoRepository<VirtualPet, String> {
+    Mono<VirtualPet> findByName(String name);
+}
