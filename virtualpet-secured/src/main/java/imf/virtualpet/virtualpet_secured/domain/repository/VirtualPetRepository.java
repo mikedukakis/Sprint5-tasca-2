43b87465789d747cdd5a756bb938e862b1adefc9
imf <imoyaf@gmail.com>
2024-10-27 05:47:43 +0100

    Develop VirtualPet repository

--- virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/repository/VirtualPetRepository.java
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/repository/VirtualPetRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface VirtualPetRepository extends ReactiveMongoRepository<VirtualPet, String> {
-}
+public interface VirtualPetRepository extends ReactiveMongoRepository<VirtualPet, String> {}
