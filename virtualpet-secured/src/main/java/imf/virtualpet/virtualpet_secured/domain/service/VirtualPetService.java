f039105cb7898d260dafb6fb5ba9f1fd5db1e9e5
imf <imoyaf@gmail.com>
2024-10-27 05:48:34 +0100

    Develop VirtualPet service

--- virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/service/VirtualPetService.java
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/service/VirtualPetService.java
@@ -1,7 +1,35 @@
 package imf.virtualpet.virtualpet_secured.domain.service;
 
+import imf.virtualpet.virtualpet_secured.domain.entity.VirtualPet;
+import imf.virtualpet.virtualpet_secured.domain.repository.VirtualPetRepository;
+import lombok.Data;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+@Data
 @Service
 public class VirtualPetService {
+    private final VirtualPetRepository virtualPetRepository;
+
+    public Mono<VirtualPet> createPet(VirtualPet pet) {
+        return virtualPetRepository.save(pet);
+    }
+
+    public Mono<VirtualPet> findPet(String petId) {
+        return virtualPetRepository.findById(petId);
+    }
+
+    public Flux<VirtualPet> findAllPets() {
+        return virtualPetRepository.findAll();
+    }
+
+//    public Mono<VirtualPet> updatePet() {
+//
+//    }
+
+    public Mono<Void> deletePet(String petId) {
+        return virtualPetRepository.deleteById(petId);
+    }
+
 }
