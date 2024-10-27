e938b0770be15c7c2217c385f4647f50f6c80e67
imf <imoyaf@gmail.com>
2024-10-27 19:54:25 +0100

    Remove unused import

--- virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/security/controller/AuthController.java
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/security/controller/AuthController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
-import org.springframework.data.mongodb.repository.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
