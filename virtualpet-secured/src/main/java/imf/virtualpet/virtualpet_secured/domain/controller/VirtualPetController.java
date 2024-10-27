683c81487d4ae7d8079181d3fc6ba9e93d134f08
imf <imoyaf@gmail.com>
2024-10-27 05:49:19 +0100

    Start developing VirtualPet controller

--- virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/controller/VirtualPetController.java
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/controller/VirtualPetController.java
@@ -1,7 +1,23 @@
 package imf.virtualpet.virtualpet_secured.domain.controller;
 
+import imf.virtualpet.virtualpet_secured.domain.entity.VirtualPet;
+import imf.virtualpet.virtualpet_secured.domain.service.VirtualPetService;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+@Data
 @RestController
 public class VirtualPetController {
+    private final VirtualPetService virtualPetService;
+
+    @PostMapping("/Create")
+//    public Mono<ResponseEntity<VirtualPet>> createPet(VirtualPet pet) {
+    public Mono<VirtualPet> createPet(VirtualPet pet) {
+        return virtualPetService.createPet(pet);
+//                .map(pet -> ResponseEntity.ok());
+    }
 }
