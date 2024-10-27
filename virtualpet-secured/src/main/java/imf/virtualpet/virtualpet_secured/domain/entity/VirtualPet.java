1c7f31e6f1cb90e000edd70f005fe25a0174aa47
imf <imoyaf@gmail.com>
2024-10-27 22:51:34 +0100

    Add interaction attributes and getRandomBool method

--- virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/entity/VirtualPet.java
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/domain/entity/VirtualPet.java
@@ -5,6 +5,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.Random;
+
 @Data
 @Document
 public class VirtualPet {
@@ -16,6 +18,23 @@ public class VirtualPet {
     private PetType petType;
     @Field("Colour")
     private String colour;
+    @Field("Hunger")
+    private boolean isHungry;
+    @Field("Happiness")
+    private boolean isHappy;
 
+    public VirtualPet(String name, PetType petType, String colour) {
+        this.name = name;
+        this.petType = petType;
+        this.colour =  colour;
+        this.isHungry = getRandomBool();
+        this.isHappy = getRandomBool();
+    }
+
+    public static boolean getRandomBool() {
+        Random randomNumber = new Random();
+        int myRandNumber = randomNumber.nextInt(2);
+        return myRandNumber == 1;
+    }
 
 }
