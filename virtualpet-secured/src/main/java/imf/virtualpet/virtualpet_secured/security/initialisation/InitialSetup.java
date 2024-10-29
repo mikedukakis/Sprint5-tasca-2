456a5d983ca28143ba0118afaca4804f3f6efbb6
imf <imoyaf@gmail.com>
2024-10-29 13:29:04 +0100

    Fixed problem wit initial setup

--- virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/security/initialisation/InitialSetup.java
+++ virtualpet-secured/src/main/java/imf/virtualpet/virtualpet_secured/security/initialisation/InitialSetup.java
@@ -8,7 +8,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import reactor.core.publisher.Mono;
 
 @Data
@@ -16,24 +15,24 @@ import reactor.core.publisher.Mono;
 public class InitialSetup {
     private static final String USER_COLLECTION = "user";
     private static final String VIRTUALPET_COLLECTION = "virtualpet";
-    private UserRegistrationDTO userRegistrationDTO;
 
     @Bean
-    public ApplicationRunner setupAdminUser(UserService userService, PasswordEncoder passwordEncoder, ReactiveMongoTemplate reactiveMongoTemplate) {
+    public ApplicationRunner setupAdminUser(UserService userService, ReactiveMongoTemplate reactiveMongoTemplate) {
         return args -> createDatabaseAndCollections(reactiveMongoTemplate)
                 .then(
                         userService.findByUsername("admin")
                                 .switchIfEmpty(
-                                        userService.registerUser(new UserRegistrationDTO("admin", "123456789", Role.ADMIN))
+                                        userService.registerUser(new UserRegistrationDTO("admin", "123456789", Role.ROLE_ADMIN))
                                 )
                 ).subscribe();
     }
 
     private Mono<Void> createDatabaseAndCollections(ReactiveMongoTemplate reactiveMongoTemplate) {
-        return reactiveMongoTemplate.createCollection(USER_COLLECTION)
-                .onErrorResume(e -> Mono.empty())
-                .then(reactiveMongoTemplate.createCollection(VIRTUALPET_COLLECTION)
-                        .onErrorResume(e -> Mono.empty()))
-                .then();
+        return reactiveMongoTemplate.collectionExists(USER_COLLECTION)
+                .flatMap(exists -> exists ? Mono.defer(Mono::empty) : reactiveMongoTemplate.createCollection(USER_COLLECTION).then())
+                .then(
+                        reactiveMongoTemplate.collectionExists(VIRTUALPET_COLLECTION)
+                                .flatMap(exists -> exists ? Mono.defer(Mono::empty) : reactiveMongoTemplate.createCollection(VIRTUALPET_COLLECTION).then())
+                );
     }
 }
